b6d66d6989098d5722ef82a4999781613bc83fba
Felix H. Dahlke <fhd@ubercode.de>
2012-09-04 16:27:12 +0200

    Initial commit

---
+++ chrome/content/crawler.js
@@ -0,0 +1,15 @@
+/*
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+function init()
+{
+  Application.console.log("init");
+}
+
+function destroy()
+{
+  Application.console.log("destroy");
+}
---
+++ prefs.js
@@ -0,0 +1,1 @@
+pref("extensions.abpwatcher.startwatching_key", "Accel Shift M, Accel Shift F4");
