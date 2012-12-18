6eaf95fd5ef84de1d9b929b701ccd8e29e2a9d5e
eric <none@none>
2012-12-17 23:02:54 -0700

    Implemented cancellation for the runner. Right now this only allows programmatic cancellation, since the task runner doesn't ever give up control to the JS interpreter. Added variation in completion status display between Finished and Cancelled.

--- chrome/content/task_test_ui.js
+++ chrome/content/task_test_ui.js
@@ -10,18 +10,34 @@ var current_task = null;
 
 function task_finished()
 {
+    if ( current_task.cancelled )
+    {
+        var status = "Cancelled";
+    }
+    else
+    {
+        status = "Finished";
+    }
+
     var status_field = document.getElementById( "task_status" );
     status_field.removeChild( status_field.childNodes[0] );
-    status_field.appendChild( document.createTextNode( "Finished" ) );
-    Task.log( "Finished" );
+    status_field.appendChild( document.createTextNode( status ) );
+    Task.log( status );
 }
 
 function task_start_click()
 {
-    Task.log( "Clicked start" );
-    current_task = new Task( Task.tg_count, task_finished );
-    var status_field = document.getElementById( "task_status" );
-    status_field.appendChild( document.createTextNode( "Started" ) );
-    Task.log( "Started" );
-    current_task.run();
+    if ( !current_task )
+    {
+        Task.log( "Clicked start" );
+        current_task = new Task( Task.tg_count, task_finished );
+        var status_field = document.getElementById( "task_status" );
+        status_field.appendChild( document.createTextNode( "Started" ) );
+        Task.log( "Started" );
+        current_task.run();
+    }
+    else
+    {
+        // We have a running task, so cancel it.
+    }
 }
