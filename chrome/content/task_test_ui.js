106afac0edd326dae515324477a707ac14494fa3
eric <none@none>
2012-12-17 20:40:40 -0700

    New file task.js contains initial version of a long-running task class. Wrote new code in task_test_ui.js to use it.
    Added status field to task test tab.
    
    Renamed click handler in the task test button.
    
    Minor refomratting in crawler_ui.js

--- chrome/content/task_test_ui.js
+++ chrome/content/task_test_ui.js
@@ -1,4 +1,27 @@
-function task_test_click()
+/*
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+var {Task} = require( "task" );
+
+var current_task = null;
+
+function task_finished()
 {
-    Cu.reportError( "Clicked" );
+    var status_field = document.getElementById( "task_status" );
+    status_field.removeChild( status_field.childNodes[0] );
+    status_field.appendChild( document.createTextNode( "Finished" ) );
+    Task.log( "Finished" );
+}
+
+function task_start_click()
+{
+    Task.log( "Clicked start" );
+    current_task = new Task( Task.tg_count, task_finished );
+    var status_field = document.getElementById( "task_status" );
+    status_field.appendChild( document.createTextNode( "Started" ) );
+    Task.log( "Started" );
+    current_task.run();
 }
