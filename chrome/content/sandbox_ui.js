a0c27e1a554eb1b5e3678b80cbc1c2bc31663d98
eric <none@none>
2013-01-26 17:37:11 -0700

    Unstable YAML_stream. Added an stop marker pseudovalue to end sequences.
    Added start and stop marker encoding types. Reworked the view specification
    functions a bit.
    
    Removed original YAML_stream code.

--- chrome/content/sandbox_ui.js
+++ chrome/content/sandbox_ui.js
@@ -24,39 +24,50 @@ function sandbox_start()
             // prelude
             Encoding.immediate_fields( ["time_start", "instruction"] ),
             // observation
-            Encoding.deferred_array( "observation" ),
+            Encoding.field( "observation", Encoding.array( false ) ),
+            //Encoding.array_deferred( "observation" ),
             // postlude
             Encoding.immediate_fields( ["time_finish", "termination"] )
         ] )
     };
-    var test_2_encoding = {
-        __encode_as__: Encoding.array_immediate()
-    };
-    var test_array = ["item 1", "item 2", "item 3"];
 
     var y = new Encoding.YAML_stream( write );
 
+    //-----------------------------------------------------------------------------------------
+    write( "---\n" );
+    write( "# 0\n" );
     y.write( "# Output generated with YAML_stream" );
+
+    //-----------------------------------------------------------------------------------------
     write( "---\n" );
     write( "# 1\n" );
+    Cu.reportError( "1." );
     var g = y.write( "Test string" );
     if ( g )
     {
         log( "1. error: immediate array spec returned non-null generator" );
     }
 
+    //-----------------------------------------------------------------------------------------
     write( "---\n" );
     write( "# 2\n" );
-    g = y.write( test_array, Encoding.array_immediate() );
+    Cu.reportError( "2." );
+    var test_array = ["item 1", "item 2", "item 3"];
+    g = y.write( test_array, Encoding.array( true ) );
     if ( g )
     {
         log( "2. error: immediate array spec returned non-null generator" );
     }
 
+    //-----------------------------------------------------------------------------------------
     write( "---\n" );
     write( "# 3\n" );
-    g = y.write( [], Encoding.array_deferred() );
-    if ( ! g )
+    Cu.reportError( "3." );
+    /*
+     * An array of deferred arrays.
+     */
+    g = y.write( [], Encoding.array( false ) );
+    if ( !g )
     {
         log( "3. error: deferred array spec returned null generator" );
     }
@@ -73,9 +84,37 @@ function sandbox_start()
             break;
         }
     }
-    log( "3. Loop end" );
-    y.write_stop();
+    y.sequence_stop();
 
+    //-----------------------------------------------------------------------------------------
+    write( "---\n" );
+    write( "# 4\n" );
+    Cu.reportError( "4." );
+    var test4_view = {
+        __view__: Encoding.array( false )
 
+    };
+
+    g = y.write( [], Encoding.array( false, Encoding.array( false ) ) );
+    for ( let i = 0 ; i < 3 ; ++i )
+    {
+        for ( let j = 0 ; j <= i ; ++j )
+        {
+            let s = "item " + ( i + 1 ) + "." + j;
+            log( "4. " + s );
+            try
+            {
+                g.send( s );
+            }
+            catch ( e )
+            {
+                if ( e !== StopIteration ) throw e;
+                log( "4. StopIteration at " + i + "," + j );
+                throw e;
+            }
+        }
+        y.sequence_stop();
+    }
+    y.sequence_stop();
 }
 
