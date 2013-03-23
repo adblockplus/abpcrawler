2dd34e70391226dc5d0ba3889a7bbe7a8b0780bb
eric <none@none>
2013-03-23 10:03:04 -0600

    Reformatted chrome/ code to 2-space indent.

--- chrome/content/task_test_ui.js
+++ chrome/content/task_test_ui.js
@@ -12,49 +12,49 @@ var current_task = null;
 
 function update_status( s, perform_log )
 {
-    var status_field = document.getElementById( "task_status" );
-    if ( status_field.childNodes.length > 0 )
-    {
-        status_field.removeChild( status_field.childNodes[0] );
-    }
-    status_field.appendChild( document.createTextNode( s ) );
-    if ( arguments.length >= 2 && perform_log )
-    {
-        log( s );
-    }
+  var status_field = document.getElementById( "task_status" );
+  if ( status_field.childNodes.length > 0 )
+  {
+    status_field.removeChild( status_field.childNodes[0] );
+  }
+  status_field.appendChild( document.createTextNode( s ) );
+  if ( arguments.length >= 2 && perform_log )
+  {
+    log( s );
+  }
 }
 
 function update_button( b )
 {
-    var button = document.getElementById( "task_go" );
-    if ( b )
-    {
-        button.label = "Cancel"
-    } else
-    {
-        button.label = "Start"
-    }
+  var button = document.getElementById( "task_go" );
+  if ( b )
+  {
+    button.label = "Cancel"
+  } else
+  {
+    button.label = "Start"
+  }
 
 }
 
 function task_finished()
 {
-    if ( current_task.cancelled )
-    {
-        var status = "Cancelled";
-    }
-    else
-    {
-        status = "Finished";
-    }
-    update_status( status );
-    update_button( false );
-    current_task = null;
+  if ( current_task.cancelled )
+  {
+    var status = "Cancelled";
+  }
+  else
+  {
+    status = "Finished";
+  }
+  update_status( status );
+  update_button( false );
+  current_task = null;
 }
 
 function task_count( n )
 {
-    update_status( "Count " + n, false );
+  update_status( "Count " + n, false );
 }
 
 /*
@@ -62,48 +62,48 @@ function task_count( n )
  */
 function task_start_click()
 {
-    if ( !current_task )
+  if ( !current_task )
+  {
+    log( "Clicked start" );
+    if ( !Counting_Task )
     {
-        log( "Clicked start" );
-        if ( !Counting_Task )
-        {
-            log( "No Counting_Task" );
-        }
-        if ( !Long_Task )
-        {
-            log( "No Long_Task" );
-        }
-        log( "require counter_task: " + require( "counter_task" ).toString() );
+      log( "No Counting_Task" );
+    }
+    if ( !Long_Task )
+    {
+      log( "No Long_Task" );
+    }
+    log( "require counter_task: " + require( "counter_task" ).toString() );
 
-        let count = document.getElementById( "task_count" ).value;
-        let limit = document.getElementById( "task_limit" ).value;
-        var variant;
-        switch ( document.getElementById( "counting_variant" ).selectedIndex )
-        {
-            case 0:
-                variant = { type: "continuous" };
-                break;
-            case 1:
-                variant = { type: "segmented fast" };
-                break;
-            case 2:
-                variant = { type: "segmented slow", interval: document.getElementById( "slow_interval" ).value};
-                break;
-            default:
-                log( "Unknown variant. This is an implementation defect." );
-                throw "bad variant";
-        }
-        current_task = new Long_Task( new Counting_Task( count, task_count, task_finished, variant ), false, limit );
-        update_status( "Started" );
-        update_button( true );
-        current_task.run();
-    }
-    else
+    let count = document.getElementById( "task_count" ).value;
+    let limit = document.getElementById( "task_limit" ).value;
+    var variant;
+    switch ( document.getElementById( "counting_variant" ).selectedIndex )
     {
-        current_task.cancel();
-        update_button( false );
-        // We have a running task, so cancel it.
+      case 0:
+        variant = { type: "continuous" };
+        break;
+      case 1:
+        variant = { type: "segmented fast" };
+        break;
+      case 2:
+        variant = { type: "segmented slow", interval: document.getElementById( "slow_interval" ).value};
+        break;
+      default:
+        log( "Unknown variant. This is an implementation defect." );
+        throw "bad variant";
     }
+    current_task = new Long_Task( new Counting_Task( count, task_count, task_finished, variant ), false, limit );
+    update_status( "Started" );
+    update_button( true );
+    current_task.run();
+  }
+  else
+  {
+    current_task.cancel();
+    update_button( false );
+    // We have a running task, so cancel it.
+  }
 }
 
 var log = (new Logger( "task_ui" )).make_log();
