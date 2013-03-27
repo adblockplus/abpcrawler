af44b6a90d222c822f80b114540621a2fba894cb
eric <none@none>
2013-03-26 18:21:10 -0600

    Added action states. Added a base class for asynchronous actions. Added end_well() and end_badly() to consistently manage their state. Reimplemented Defer_class with the new facilities. Added test points to check the state in the Defer_class unit tests.

--- test/async_platform-test.js
+++ test/async_platform-test.js
@@ -11,3 +11,13 @@ Async_Platform.dispatch = function( f )
 {
   return setTimeout( f, 0 );
 };
+
+Async_Platform.set_timer = function( f, duration )
+{
+  return setTimeout( f, duration )
+};
+
+Async_Platform.clear_timer = function( id )
+{
+  clearTimeout( id );
+};
