2e3b998aa4aec01d9f0c1d3708ddbe9d2abf1a12
eric <none@none>
2013-03-27 10:24:43 -0600

    Renamed Async.Timer to Async.Delay. Better consistency with other action names, which are action verbs.
    Factored out generic tests simple_finally() and simple_catch(). Implemented all
    Delay and Defer tests with these.

--- test/test-async.js
+++ test/test-async.js
@@ -27,6 +27,10 @@ Async.Asynchronous_Action.prototype.get_state = function()
 //-------------------------------------------------------
 // Generic tests
 //-------------------------------------------------------
+function verify_phase()
+{
+}
+
 /**
  * Check that an action executes its body and returns. Generic for simple, non-compound actions.
  *
@@ -53,12 +57,115 @@ function simple_try( factory, queue )
     assertEquals( 0, sequence );
   } );
 
-  queue.call( "Finish phase.", function()
+  queue.call( "End phase.", function()
   {
     assertEquals( 1, sequence );
     assertEquals( "action state is not 'Done'.", Async.Action.State.Done, d.get_state() );
   } );
 }
+
+/**
+ * Check that an action executes its body and the finisher function and returns. Generic for simple, non-compound
+ * actions. Verifying execution is done with both direct and indirect means.
+ *
+ * @param {function} factory
+ *    A factory function that yields an action.
+ * @param queue
+ */
+function simple_finally( factory, queue )
+{
+  /**
+   * @type {Async.Asynchronous_Action}
+   */
+  var d;
+  var sequence = 0;
+
+  function catcher()
+  {
+    fail( "Action under test should not throw an exception nor call its catcher." );
+  }
+
+  function finisher()
+  {
+    sequence += 2;
+  }
+
+  queue.call( "Go phase.", function( callbacks )
+  {
+    var trial = callbacks.add( function()
+    {
+      sequence += 1;
+    } );
+    d = factory( trial );
+    assertEquals( 0, sequence );
+    var monitored_finisher = callbacks.add( finisher );
+    d.go( monitored_finisher, catcher );
+  } );
+
+  queue.call( "End phase.", function()
+  {
+    assertEquals( 3, sequence );
+    assertEquals( "action state is not 'Done'.", Async.Action.State.Done, d.get_state() );
+  } );
+}
+
+/**
+ * Run a simple trial, one that throws an exception, with both finally and catch functions. Indirectly verify that all
+ * three run, but directly verify only the finally and catch.
+ *
+ * Generic for simple non-compound actions.
+ *
+ * @param {function} factory
+ *    A factory function that yields an action.
+ * @param queue
+ */
+function simple_catch( factory, queue )
+{
+  /**
+   * @type {Async.Asynchronous_Action}
+   */
+  var d;
+  var sequence = 0;
+
+  function trial()
+  {
+    assertEquals( 0, sequence );
+    sequence += 1;
+    throw new Error( "This error is part of the test." );
+  }
+
+  function catcher()
+  {
+    assertEquals( 1, sequence );
+    sequence += 2;
+  }
+
+  function finisher()
+  {
+    assertEquals( 3, sequence );
+    sequence += 4;
+  }
+
+  queue.call( "Go phase.", function( callbacks )
+  {
+    /* If we monitor the trial by adding to the callback list, it will report the exception as an error, which is not
+     * what we want. We indirectly test that it runs by incrementing the sequence number.
+     */
+    d = factory( trial );
+    var monitored_catch = callbacks.add( catcher );
+    var monitored_finally = callbacks.add( finisher );
+    assertEquals( 0, sequence );
+    d.go( monitored_finally, monitored_catch );
+    assertEquals( 0, sequence );
+  } );
+
+  queue.call( "End phase.", function()
+  {
+    assertEquals( 7, sequence );
+    assertEquals( "action state is not 'Exception'.", Async.Action.State.Exception, d.get_state() );
+  } );
+}
+
 
 //-------------------------------------------------------
 // Defer
@@ -73,10 +180,21 @@ function defer_factory( trial )
   return new Async.Defer( trial );
 }
 
-AsyncTest.prototype.test_defer_tries = function( queue )
+AsyncTest.prototype.test_defer_try = function( queue )
 {
   simple_try( defer_factory, queue );
 };
+
+AsyncTest.prototype.test_defer_finally = function( queue )
+{
+  simple_finally( defer_factory, queue );
+};
+
+AsyncTest.prototype.test_defer_catch = function( queue )
+{
+  simple_catch( defer_factory, queue );
+};
+
 
 /**
  * Run a dispatch trial by itself. Verifies that the trial runs both directly, by registering it in the callback list,
@@ -113,7 +231,7 @@ AsyncTest.prototype.test_defer_tries_OLD = function( queue )
  * direct and indirect means.
  * @param queue
  */
-AsyncTest.prototype.test_defer_finalizes = function( queue )
+AsyncTest.prototype.test_defer_finalizes_OLD = function( queue )
 {
   var d;
   var sequence = 0;
@@ -147,7 +265,7 @@ AsyncTest.prototype.test_defer_finalizes = function( queue )
  * three run, but directly verify only the finally and catch.
  * @param queue
  */
-AsyncTest.prototype.test_defer_catches = function( queue )
+AsyncTest.prototype.test_defer_catches_OLD = function( queue )
 {
   var d;
   var sequence = 0;
@@ -188,25 +306,39 @@ AsyncTest.prototype.test_defer_catches = function( queue )
 };
 
 //-------------------------------------------------------
-// Timer
+// Delay
 //-------------------------------------------------------
 
 /**
- * Factory for Timer objects
+ * Factory for Delay objects
  * @param delay
  * @param trial
- * @return {Async.Timer}
+ * @return {Async.Delay}
  */
-function timer_factory( delay, trial )
+function delay_factory( delay, trial )
 {
-  return new Async.Timer( trial, delay );
+  return new Async.Delay( trial, delay );
 }
 
-AsyncTest.prototype.test_timer_tries = function( queue )
+function simple_delay_factory( trial )
 {
-  simple_try( timer_factory.bind( null, 2 ), queue );
+  return delay_factory( 2, trial );
+}
+
+AsyncTest.prototype.test_delay_tries = function( queue )
+{
+  simple_try( simple_delay_factory, queue );
 };
 
+AsyncTest.prototype.test_delay_finally = function( queue )
+{
+  simple_finally( simple_delay_factory, queue );
+};
+
+AsyncTest.prototype.test_delay_catch = function( queue )
+{
+  simple_catch( simple_delay_factory, queue );
+};
 
 
 /**
@@ -225,7 +357,7 @@ AsyncTest.prototype.test_timer_tries_OLD = function( queue )
     {
       sequence += 1;
     } );
-    d = new Async.Timer( trial, 2 );
+    d = new Async.Delay( trial, 2 );
     assertEquals( Async.Action.State.Ready, d.get_state() );
     assertEquals( 0, sequence );
     d.go();
